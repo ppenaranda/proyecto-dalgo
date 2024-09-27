4be1360e32087f070e928dbf6755c29c5c4b5924
Franisis <f.guzmana@uniandes.edu.co>
2024-09-27 17:45:07 -0500

    Finished Marion and Indiana

--- proyecto1.py
+++ proyecto1.py
@@ -1,7 +1,7 @@
 numeros = [
     [0, 9, 1, 10, 0],
-    [11, 11, 5, 7, 5],
-    [1, -1, 3, 1, -1],
+    [1, 1, 5, 5, -1],
+    [1, -1, 1, 1, -1],
     [5, 5, 5, 15, 2],
     [55, 3, 0, 4, 1]
 ]
@@ -19,13 +19,16 @@ def exploracion(piramide):
     prioridad tiene un explorador con otro. 
 
     """
-    puntoDeEncuentro= piramide[len(piramide)//2]
+    
     
     """
     Se hará en top bottom desde el punto de encuentro reduciendo de a una las opciones y sumando
     """
     
     for paso in range(len(piramide)//2,-1,-1):
+        if piramide[1][1] == -1 and piramide[1][0]==-1 and piramide[1][len(piramide[0])-1]==-1 and piramide[1][len(piramide[0])-2]==-1 :
+            
+            break
         if paso == 0:
             simulacionDeCeldas[0][0] = max(simulacionDeCeldas[1][0], simulacionDeCeldas[1][1])
             simulacionDeCeldas[0][len(piramide[0])-1] = max(simulacionDeCeldas[1][len(piramide[0])-1], simulacionDeCeldas[1][len(piramide[0])-2])
@@ -60,26 +63,28 @@ def exploracion(piramide):
                             mitadEnteraDelPasoMarion[len(mitadEnteraDelPasoMarion)-1]+ opcionesPasoAnteriorMarion[celdaMarion], 
                             maximo
                             )
-                    
+                    print(maximo)
                     simulacionDeCeldas[paso-1][len(piramide[paso])-celdaMarion]= maximo
                 
                 elif celdaMarion < len(opcionesPasoAnteriorMarion)-1:
                     maximo = 0
-                    
-                    if mitadEnteraDelpaso[celdaMarion-1]>-1:
-                        maximo = max(
-                            mitadEnteraDelPasoMarion[celdaMarion-1]+opcionesPasoAnteriorMarion[celdaMarion], 
-                            maximo)
-                        
-                    if mitadEnteraDelpaso[celdaMarion]>-1:
-                        maximo = max(
-                            mitadEnteraDelPasoMarion[celdaMarion]+opcionesPasoAnteriorMarion[celdaMarion], 
-                            maximo) 
-                        
-                    if mitadEnteraDelpaso[celdaMarion+1]>-1:
-                        maximo = max(
-                            mitadEnteraDelPasoMarion[celdaMarion+1]+opcionesPasoAnteriorMarion[celdaMarion], 
-                                     maximo)
+                    if piramide[paso-1][len(simulacionDeCeldas[paso-1])//2+celdaMarion+1]>=0:
+
+                        if mitadEnteraDelpaso[celdaMarion-1]>=0:
+                            maximo = max(
+                                mitadEnteraDelPasoMarion[celdaMarion-1]+opcionesPasoAnteriorMarion[celdaMarion], 
+                                maximo)
+                            
+                        if mitadEnteraDelpaso[celdaMarion]>=0:
+                            maximo = max(
+                                mitadEnteraDelPasoMarion[celdaMarion]+opcionesPasoAnteriorMarion[celdaMarion], 
+                                maximo) 
+                            
+                        if mitadEnteraDelpaso[celdaMarion+1]>=0:
+                            maximo = max(
+                                mitadEnteraDelPasoMarion[celdaMarion+1]+opcionesPasoAnteriorMarion[celdaMarion], 
+                                        maximo)
+                    print(maximo, 'marion?')
                     simulacionDeCeldas[paso-1][len(simulacionDeCeldas[paso-1])//2+celdaMarion+1]=maximo
                 elif celdaMarion==0 and len(simulacionDeCeldas[0])==len(mitadEnteraDelPasoMarion):
                     """
