344f38f889af3c2fc57cfb993e76806536ba9908
Franisis <f.guzmana@uniandes.edu.co>
2024-09-27 21:59:45 -0500

    62+2

--- proyecto1.py
+++ proyecto1.py
@@ -1,8 +1,14 @@
+import sys
+
+
+
+
+
 numeros = [
     [0, 9, 1, 10, 0],
     [-1, 5, 5, 25, 5],
-    [1, 5, 1, 5, 7],
-    [5, 5, 5, 15, 2],
+    [1, 14, 12, 5, 7],
+    [5, 5, 4, 15, 2],
     [55, 3, 0, 4, 1]
 ]
 def remove_extremes(lst, m):
@@ -25,6 +31,62 @@ def exploracion(piramide):
     """
     Se hará en top bottom desde el punto de encuentro reduciendo de a una las opciones y sumando
     """
+
+    
+    for pasosSallah in range((len(piramide)//2)-1, len(piramide), 1 ):
+        pasoRealSallah = (len(piramide)//2) + pasosSallah
+        if pasoRealSallah > len(piramide)-1:
+            break
+        #verificamos si son solo rectanngulos primero
+        if pasoRealSallah < len(piramide) and len(simulacionDeCeldas[0])>=pasoRealSallah:
+            pasoSiguienteSallah = remove_extremes(piramide[pasoRealSallah], pasosSallah)
+            pasoActualSallah = piramide[pasoRealSallah-1]
+            
+            for celda in range(len(pasoSiguienteSallah)):
+                if celda == 0 :
+                    """
+                    Hallar el maximo del medio
+                    """
+                    maximo = 0
+                    
+                    if pasoActualSallah[celda]>0:
+                        maximo = max(maximo, pasoActualSallah[celda]+pasoSiguienteSallah[celda])
+                    if pasoActualSallah[celda+1]>0:
+                        maximo = max(maximo, pasoActualSallah[celda+1]+pasoSiguienteSallah[celda])
+                    if pasoActualSallah[celda+2]>0:
+                        maximo = max(maximo, pasoActualSallah[celda+2]+pasoSiguienteSallah[celda])
+                    
+                    simulacionDeCeldas[pasoRealSallah][celda+1] = maximo
+                    piramide[pasoRealSallah][celda+1] = 0
+                elif 0<celda<len(pasoSiguienteSallah)-1:
+                    maximo=0
+                    
+                    if pasoActualSallah[celda-1]>0:
+                        maximo = max(maximo, pasoActualSallah[celda]+pasoSiguienteSallah[celda])
+                        
+                    if pasoActualSallah[celda]>0:
+                        maximo = max(maximo, pasoActualSallah[celda+1]+pasoSiguienteSallah[celda])
+                        
+                    if pasoActualSallah[celda+1]>0:
+                        maximo = max(maximo, pasoActualSallah[celda+2]+pasoSiguienteSallah[celda])
+                
+                    simulacionDeCeldas[pasoRealSallah][celda+1] = maximo
+                    piramide[pasoRealSallah][celda+1] = 0
+
+                elif celda == len(pasoSiguienteSallah)-1:
+                    maximo = 0
+                    if pasoActualSallah[celda]>0:
+                        
+                        maximo = max(maximo, pasoActualSallah[celda]+pasoSiguienteSallah[celda])
+                    if pasoActualSallah[celda+1]>0:
+                        
+                        maximo = max(maximo, pasoActualSallah[celda+1]+pasoSiguienteSallah[celda])
+                    if pasoActualSallah[celda+2]>0:
+                        
+                        maximo = max(maximo, pasoActualSallah[celda+2]+pasoSiguienteSallah[celda])
+                    
+                    simulacionDeCeldas[pasoRealSallah][celda+1] = maximo
+                    piramide[pasoRealSallah][celda+1] = 0
     
     for paso in range(len(piramide)//2,-1,-1):
         if piramide[1][1] == -1 and piramide[1][0]==-1 and piramide[1][len(piramide[0])-1]==-1 and piramide[1][len(piramide[0])-2]==-1:
@@ -32,6 +94,7 @@ def exploracion(piramide):
         if paso == 0:
             simulacionDeCeldas[0][0] = max(simulacionDeCeldas[1][0], simulacionDeCeldas[1][1])
             simulacionDeCeldas[0][len(piramide[0])-1] = max(simulacionDeCeldas[1][len(piramide[0])-1], simulacionDeCeldas[1][len(piramide[0])-2])
+            
         if paso > 0:
             columnas = len(piramide[0])
             
@@ -52,11 +115,13 @@ def exploracion(piramide):
                 if celdaMarion == len(opcionesPasoAnteriorMarion)-1 and opcionesPasoAnteriorMarion[celdaMarion] > 0:
                     
                     maximo = 0
+
                     if mitadEnteraDelPasoMarion[len(mitadEnteraDelPasoMarion)-2]>0:
                         maximo = max(
                             mitadEnteraDelPasoMarion[len(mitadEnteraDelPasoMarion)-2]+ opcionesPasoAnteriorMarion[celdaMarion], 
                                      maximo
                                      )
+                        
                     if mitadEnteraDelPasoMarion[len(mitadEnteraDelPasoMarion)-1]>0:
                         maximo = max(
                             mitadEnteraDelPasoMarion[len(mitadEnteraDelPasoMarion)-1]+ opcionesPasoAnteriorMarion[celdaMarion], 
@@ -64,6 +129,7 @@ def exploracion(piramide):
                             )
                     
                     simulacionDeCeldas[paso-1][len(piramide[paso])-celdaMarion]= maximo
+                    piramide[paso-1][len(piramide[paso])-celdaMarion]=0
                 
                 elif celdaMarion < len(opcionesPasoAnteriorMarion)-1:
                     maximo = 0
@@ -85,6 +151,7 @@ def exploracion(piramide):
                                         maximo)
                     
                     simulacionDeCeldas[paso-1][len(simulacionDeCeldas[paso-1])//2+celdaMarion+1]=maximo
+                    piramide[paso-1][len(simulacionDeCeldas[paso-1])//2+celdaMarion+1]=0
                 elif celdaMarion==0 and len(simulacionDeCeldas[0])==len(mitadEnteraDelPasoMarion):
                     """
                     Este solo sirve si falta poco
@@ -103,8 +170,8 @@ def exploracion(piramide):
                         )
                     
                     simulacionDeCeldas[paso-1][1]=maximo
-                elif celdaMarion==0 and len(simulacionDeCeldas[0])<len(opcionesPasoAnteriorMarion):
-                    pass                   
+                    piramide[paso-1][1]=0
+                             
                             
             for celda in range(len(opcionesPasoAnterior)):
                 if celda == 0 and opcionesPasoAnterior[celda]>0: #Esta pegado a la pared 
@@ -114,6 +181,7 @@ def exploracion(piramide):
                     if mitadEnteraDelpaso[1]>0:
                         maximo = max(mitadEnteraDelpaso[1]+opcionesPasoAnterior[celda], maximo)
                     simulacionDeCeldas[paso-1][celda] = maximo
+                    piramide[paso-1][celda] = 0
                     
                 elif len(opcionesPasoAnterior)+1>celda > 0 and paso > 0 and opcionesPasoAnterior[celda]>0:
                     #Cuando esta en todo el medio
@@ -128,56 +196,40 @@ def exploracion(piramide):
                     
 
                     simulacionDeCeldas[paso-1][celda] = maximo
+                    piramide[paso-1][celda] = 0
                 elif celda == len(mitadEnteraDelpaso)-1: # cuando indiana esta pegado en la otra pared
                     if mitadEnteraDelpaso[celda-1]>-1:
                         maximo = max(maximo,mitadEnteraDelpaso[celda-1]+opcionesPasoAnterior[celda])
                     if mitadEnteraDelpaso[celda]>-1:
                         maximo = max(maximo,mitadEnteraDelpaso[celda]+opcionesPasoAnterior[celda])
                     simulacionDeCeldas[paso-1][celda]=maximo
+                    piramide[paso-1][celda]=0
 
-    for pasosSallah in range((len(piramide)//2)-1, len(piramide), 1 ):
-        pasoRealSallah = (len(piramide)//2) + pasosSallah
-        if pasoRealSallah > len(piramide)-1:
-            break
-        #verificamos si son solo rectanngulos primero
-        if pasoRealSallah < len(piramide) and len(piramide[0])>pasoRealSallah:
-            pasoSiguienteSallah = piramide[pasoRealSallah]
-            pasoActualSallah = piramide[pasosSallah+1]
-            for celdaSallah in range(len(pasoSiguienteSallah)):
-                if celdaSallah == 0:
-                    maximo = 0
-                    print(pasoSiguienteSallah, 'paso siguiente de sallah')
-                    if pasoSiguienteSallah[celdaSallah+1]>=0:
-                        maximo = max(maximo, 
-                                     pasoSiguienteSallah[celdaSallah+1]+piramide[pasoRealSallah][celda-1])
-                    if pasoSiguienteSallah[celdaSallah]>=0:
-                        maximo = max(maximo, 
-                                     pasoSiguienteSallah[celdaSallah+1]+piramide[pasoRealSallah][celda])
-                    print(maximo)
-                    simulacionDeCeldas[pasoRealSallah][celdaSallah] = maximo
-                    
-
-                
-        elif True:
-            pass        
-                    
-
-                
-
-
-            
-            
-
+    simulacionDeCeldas[len(simulacionDeCeldas)-1][len(simulacionDeCeldas)//2]=max(
+        simulacionDeCeldas[len(simulacionDeCeldas)-2][(len(simulacionDeCeldas)//2)-1],
+        simulacionDeCeldas[len(simulacionDeCeldas)-2][(len(simulacionDeCeldas)//2)],
+        simulacionDeCeldas[len(simulacionDeCeldas)-2][(len(simulacionDeCeldas)//2)+1],
+    )   
         
-
-                    
-                    
-                    
-                                     
-
-    for ll in simulacionDeCeldas:
-        print(ll)
-    return max(simulacionDeCeldas[0][0], 
-               simulacionDeCeldas[0][len(simulacionDeCeldas[0])-1] )     
+    print(
+        simulacionDeCeldas[0][0],
+        simulacionDeCeldas[len(simulacionDeCeldas)-1][len(simulacionDeCeldas)//2],
+        simulacionDeCeldas[0][len(simulacionDeCeldas[0])-1]
+            )        
+         
+            
+    return (simulacionDeCeldas[0][0] +simulacionDeCeldas[0][len(simulacionDeCeldas[0])-1]+simulacionDeCeldas[len(simulacionDeCeldas)-1][len(simulacionDeCeldas)//2])     
                            
-print(exploracion(numeros))
+
+
+numerocasos = int(sys.stdin.readline())
+
+for _ in range(numerocasos):
+    r, c = map(int, sys.stdin.readline().strip().split())
+
+    matriz = []
+    for _  in range(r):
+        fila = list(map(int, sys.stdin.readline().split()))
+        matriz.append(fila)
+
+    print(exploracion(matriz))
